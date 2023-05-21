ddfe08b53e265e5bd48df6f90f534c7212e78747
Steven Pate <steven@laac.dev>
2023-05-21 12:54:12 -0500

    updating sample project urls for new style django

--- sample_project/urls.py
+++ sample_project/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 
 from sample_project import views
 
 urlpatterns = [
-    url(r"^protected/$", views.ProtectedView.as_view(), name="protected_view"),
-    url(r"^public/$", views.PublicView.as_view(), name="public_view"),
-    url(r"^public2/$", views.PublicView2.as_view(), name="public_view2"),
-    url(r"^public3/$", views.public_view3, name="public_view3"),
+    path("protected/", views.ProtectedView.as_view(), name="protected_view"),
+    path("public/", views.PublicView.as_view(), name="public_view"),
+    path("public2/", views.PublicView2.as_view(), name="public_view2"),
+    path("public3/", views.public_view3, name="public_view3"),
 ]
